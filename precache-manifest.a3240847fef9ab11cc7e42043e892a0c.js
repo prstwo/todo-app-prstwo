268403b36c814b42e7f1de0c9999884244b98cba
prstwo <prs2.pari@gmail.com>
2020-09-27 14:49:13 +0330

    Updates

---
+++ precache-manifest.a3240847fef9ab11cc7e42043e892a0c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "53b8a17a3aafe57a5480",
+    "url": "/todo-app-prstwo/static/css/main.49df8288.chunk.css"
+  },
+  {
+    "revision": "53b8a17a3aafe57a5480",
+    "url": "/todo-app-prstwo/static/js/main.53b8a17a.chunk.js"
+  },
+  {
+    "revision": "140157ea99587e064d61",
+    "url": "/todo-app-prstwo/static/js/1.140157ea.chunk.js"
+  },
+  {
+    "revision": "bc2afda79eb02f17a611",
+    "url": "/todo-app-prstwo/static/js/runtime~main.bc2afda7.js"
+  },
+  {
+    "revision": "6f93c0889d301edf16070c6775063620",
+    "url": "/todo-app-prstwo/index.html"
+  }
+];
